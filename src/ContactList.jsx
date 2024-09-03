e8b9210d0f455cdfcaf89ba4100a67b1040b7f35
thsk3 <bumakker@gmail.com>
2024-09-04 00:17:00 +0900

    feat: 연락처 목록 검색 기능 추가

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /** 추가해야 할 것
  * 1. 수정 기능 추가
  * 2. 리스트 검색 기능 추가
@@ -6,8 +8,12 @@
  */
 export default function ContactList({ contactList, setContactList, setIndex, setShowDetail }) {
     let keyWord = "";
+    let [isSearchList, setIsSearchList] = useState(false);
+    let [searchList, setSearchList] = useState([]);
     const searchContact = () => {
-        console.log(keyWord);
+        let resultList = contactList.reduce((a, c) => (c.name.includes(keyWord) ? [...a, c] : a), []);
+        setIsSearchList(true);
+        setSearchList(resultList);
     };
     const showDetail = (i) => {
         setIndex(i);
@@ -28,39 +34,71 @@ export default function ContactList({ contactList, setContactList, setIndex, set
                         keyWord = e.target.value;
                     }}
                     onKeyDown={(e) => {
-                        e.preventDefault();
                         if (e.key === "Enter") {
+                            e.preventDefault();
                             searchContact();
                         }
                     }}></input>
-                <button>전체리스트 보기</button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setIsSearchList(false);
+                    }}>
+                    전체리스트 보기
+                </button>
             </form>
             <ul className="contact-list">
-                {contactList.map((item, i) => {
-                    return (
-                        <li key={i}>
-                            <p>
-                                <span>{item.name}</span>
-                                <span>{item.number}</span>
-                                <span>{item.group}</span>
-                            </p>
-                            <p className="contact-list-btn">
-                                <span
-                                    onClick={() => {
-                                        showDetail(i);
-                                    }}>
-                                    세부사항
-                                </span>
-                                <span
-                                    onClick={() => {
-                                        deleteContact(i);
-                                    }}>
-                                    삭제
-                                </span>
-                            </p>
-                        </li>
-                    );
-                })}
+                {isSearchList
+                    ? searchList.map((item, i) => {
+                          return (
+                              <li key={i}>
+                                  <p>
+                                      <span>{item.name}</span>
+                                      <span>{item.number}</span>
+                                      <span>{item.group}</span>
+                                  </p>
+                                  <p className="contact-list-btn">
+                                      <span
+                                          onClick={() => {
+                                              showDetail(i);
+                                          }}>
+                                          세부사항
+                                      </span>
+                                      <span
+                                          onClick={() => {
+                                              deleteContact(i);
+                                          }}>
+                                          삭제
+                                      </span>
+                                  </p>
+                              </li>
+                          );
+                      })
+                    : contactList.map((item, i) => {
+                          return (
+                              <li key={i}>
+                                  <p>
+                                      <span>{item.name}</span>
+                                      <span>{item.number}</span>
+                                      <span>{item.group}</span>
+                                  </p>
+                                  <p className="contact-list-btn">
+                                      <span
+                                          onClick={() => {
+                                              showDetail(i);
+                                          }}>
+                                          세부사항
+                                      </span>
+                                      <span
+                                          onClick={() => {
+                                              deleteContact(i);
+                                          }}>
+                                          삭제
+                                      </span>
+                                  </p>
+                              </li>
+                          );
+                      })}
             </ul>
         </div>
     );
